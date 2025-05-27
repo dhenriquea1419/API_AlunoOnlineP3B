1f048036b000b15f26832b1d3c4a8a798ded387f
danielhenrique <165221001+dhenriquea1419@users.noreply.github.com>
2025-05-27 16:02:39 -0300

    Update MatriculaAlunoController.java

--- api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
+++ api/src/main/java/br/com/alunoonline/api/controller/MatriculaAlunoController.java
@@ -16,27 +16,35 @@ public class MatriculaAlunoController {
     MatriculaAlunoService matriculaAlunoService;
 
     @PostMapping
+    // responde a requisições POST para criar uma matrícula.
     @ResponseStatus(HttpStatus.CREATED)
+    //Retorna status 201 (Created).
     public void criarMatricula(@RequestBody MatriculaAluno matriculaAluno){
         matriculaAlunoService.criarMatricula(matriculaAluno);
     }
 
     @PatchMapping("/trancar/{idMatricula}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    //Retorna HTTP 204 (No Content).
     public void trancarMatricula(@PathVariable Long idMatricula) {
         matriculaAlunoService.trancarMatricula(idMatricula);
+        //Chama o serviço que faz a validação e altera o status para "trancado".
     }
 
     @PatchMapping("/atualiza-notas/{idMatricula}")
+    //PATCH para atualizar notas da matrícula com ID recebido na URL.
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    //Retorna HTTP 204.
     public void atualizaNotas(@RequestBody AtualizarNotasRequestDTO request,
                               @PathVariable Long idMatricula)
+            //Recebe o DTO AtualizarNotasRequestDTO que contém as notas no corpo da requisição.
     {
         matriculaAlunoService.atualizarNotas(request, idMatricula);
     }
 
     @GetMapping("/emitir-historico/{alunoId}")
     @ResponseStatus(HttpStatus.OK)
+    //Status HTTP 200 OK.
     public HistoricoAlunoResponseDTO emitirHistorico(@PathVariable Long alunoId) {
         return matriculaAlunoService.emitirHistorico(alunoId);
     }
