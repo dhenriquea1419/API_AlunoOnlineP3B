3c66bfccfde786dadc9bde135fea8bd61978b713
danielhenrique <165221001+dhenriquea1419@users.noreply.github.com>
2025-05-27 16:10:40 -0300

    Update DisciplinaRepository.java

--- api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
+++ api/src/main/java/br/com/alunoonline/api/repository/DisciplinaRepository.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+@Repository //Ajuda o Spring a identificar essa interface como repositório.
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long>
+//Herda automaticamente métodos como save(), findAll(), findById(), deleteById(), etc.
+{
     List<Disciplina> findByProfessorIdProfessor(Long idProfessor);
+
 }
 
